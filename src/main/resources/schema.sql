f799090ff5a15da2f6fb085edb96be8d6969c36c
Manuel Silva <manuel.silvag1@gmail.com>
2024-11-10 23:27:45 -0500

    Updated actuator and database configuration

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,13 @@
-CREATE TABLE IF NOT EXISTS `loans` (
-    `loan_id` INT AUTO_INCREMENT,
-    `customer_id` INT NOT NULL,
-    `loan_number` VARCHAR(100) UNIQUE NOT NULL,
-    `loan_type` VARCHAR(100) NOT NULL,
-    `total_loan` DECIMAL NOT NULL,
-    `amount_paid` DECIMAL NOT NULL,
-    `outstanding_amount` DECIMAL NOT NULL,
-    `created_at` DATE NOT NULL,
-    `created_by` VARCHAR(20) NOT NULL,
-    `updated_at` DATE DEFAULT NULL,
-    `updated_by` VARCHAR(20) DEFAULT NULL,
-    PRIMARY KEY (`loan_id`)
+CREATE TABLE IF NOT EXISTS loans (
+    loan_id serial primary key,
+    customer_id INT NOT NULL,
+    loan_number VARCHAR(100) UNIQUE NOT NULL,
+    loan_type VARCHAR(100) NOT NULL,
+    total_loan DECIMAL NOT NULL,
+    amount_paid DECIMAL NOT NULL,
+    outstanding_amount DECIMAL NOT NULL,
+    created_at DATE NOT NULL,
+    created_by VARCHAR(20) NOT NULL,
+    updated_at DATE DEFAULT NULL,
+    updated_by VARCHAR(20) DEFAULT NULL
 );
